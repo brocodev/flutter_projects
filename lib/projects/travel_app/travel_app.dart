53258b688d03c3da21b0af924edd4fb7f23d58ef
Kevin Melendez <kevinmdezhdez@gmail.com>
2021-11-05 01:12:08 -0600

    reorganize assets

--- lib/projects/travel_app/travel_app.dart
+++ lib/projects/travel_app/travel_app.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TravelApp extends StatelessWidget {
   const TravelApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola!!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('New York'),
+        ),
       ),
     );
   }
